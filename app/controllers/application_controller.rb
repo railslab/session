c1645725dd04dcdcadbf476776179c295f22aac8
Marcos Neves <marcos.neves@gmail.com>
2014-03-19 08:54:48 -0300

    usuario_logado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :logado?
+  helper_method :usuario_logado
 
   def logado?
-    session[:id].present?
+    session_id.present?
   end
 
   def acesso_restrito!
     redirect_to login_path unless logado?
-  end  
+  end
+
+  def session_id
+  	session[:id]
+  end
+
+  def usuario_logado
+  	Cadastro.find session_id rescue nil
+  end
 end
